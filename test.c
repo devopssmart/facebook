7bab479ca9b6a57ba4dfaa80209e8890762d4f28
Mahesh <devopssmart493@gmail.com>
2023-01-05 19:10:55 +0530

    Update test.c

--- test.c
+++ test.c
@@ -4,3 +4,5 @@ my name is mahesh muppalla
 i am from andhrapradesh 
 coming to bengaluru
 your welcomr
+updated coede is geeting ready for the position in the stream of devops
+update this file for request demo
